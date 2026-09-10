1d4dcbfb6660558400b17d735e27bd339eb7de4d
Sheldon De Sousa <sheldondesousa@Sheldons-MacBook-Pro.local>
2026-09-10 18:18:08 +0530

    Use dark status bar icons

--- app/src/main/java/com/sheldondesousa/uncork/MainActivity.kt
+++ app/src/main/java/com/sheldondesousa/uncork/MainActivity.kt
@@ -2,6 +2,7 @@ package com.sheldondesousa.uncork
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.SystemBarStyle
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.runtime.getValue
@@ -19,7 +20,12 @@ class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+        enableEdgeToEdge(
+            statusBarStyle = SystemBarStyle.light(
+                scrim = android.graphics.Color.TRANSPARENT,
+                darkScrim = android.graphics.Color.TRANSPARENT,
+            ),
+        )
         val modelFileManager = ModelFileManager(applicationContext)
         gemmaResponder = GemmaConversationResponder(applicationContext, modelFileManager.modelFile)
         setContent {
